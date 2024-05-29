87d7d8f5743d26d88c982cc7de2e2b6382f336da
JBHarrison5 <john.harrison147@virginmedia.com>
2024-05-29 12:56:49 +0100

    finished applicatin

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ const table = document.querySelector('table');
 const bibSort = document.querySelector('#bibSort')
 const rankSort = document.querySelector('#rankSort')
 const exportCSVBtn = document.querySelector('#exportCSV')
+const raceNameDisplay = document.querySelector('#raceNameDisplay')
+const raceDateDisplay = document.querySelector('#raceDateDisplay')
+const raceDistanceDisplay = document.querySelector('#raceDistanceDisplay')
+const raceGenderDisplay = document.querySelector('#raceGenderDisplay')
 
+
+let bibSortCount = 0
+let rankSortCount = 0
+let raceDetails = []
 let athletesArray = []
+
+fetch("MarathonResults.json")
+    .then(res => res.json())
+    .then(data => {
+        athletesArray = createAthletesArray(data['results']['athletes'])
+        raceDetails = createRaceDetailsArray(data['results'])
+        displayRaceDetails(raceDetails)
+        displayAthletes(athletesArray)
+    })
+
 const createAthletesArray = (results) => {
     let athletes_data = []
     for (const athlete of results) {
@@ -19,10 +37,22 @@ const createAthletesArray = (results) => {
     return athletes_data
 }
 
+const createRaceDetailsArray = (results) => {
+    let array = []
+    array['raceName'] = results['racename'];
+    array['dateTime'] = results['tod'].slice(11,19) + ' on ' + results['tod'].slice(8,10) + '-'
+        + results['tod'].slice(5,7) + '-' + results['tod'].slice(0,4)
+    array['distance'] = results['racelength'] + ' Miles '
+    array['gender'] = results['gender'].charAt(0).toUpperCase() + results['gender'].slice(1)
+    return array
+}
+
 const exportCSVFile = (athletesArray) => {
     let CSVArray = [['Rank', 'Full Name', 'Finish Time', 'Country Code']];
     for (let athlete of athletesArray) {
-        let rowArray = [athlete['rank'], athlete['fullName'], athlete['finishTime'], athlete['countryCode']]
+        let athleteName = athlete['fullName'].split(' ')[0] + ' ' +
+            athlete['fullName'].split(' ')[1].toLowerCase().charAt(0).toUpperCase() + athlete['fullName'].split(' ')[1].toLowerCase().slice(1)
+        let rowArray = [athlete['rank'], athleteName, athlete['finishTime'], athlete['countryCode']]
         CSVArray.push(rowArray)
     }
     let csvContent = '';
@@ -38,13 +68,20 @@ const exportCSVFile = (athletesArray) => {
 const displayAthletes = (athletes) => {
     for (const athlete of athletes) {
         let row = document.createElement('tr');
-        for (const [key, value] of Object.entries(athlete)) {
+        for (const [_, value] of Object.entries(athlete)) {
             let data = document.createElement('td');
             data.textContent = value;
             row.appendChild(data)
         }
         table.appendChild(row)
     }
+}
+
+const displayRaceDetails = (raceDetails) => {
+    raceNameDisplay.innerText = raceDetails['raceName'];
+    raceDateDisplay.innerText = 'Date: ' + raceDetails['dateTime'];
+    raceDistanceDisplay.innerText = 'Distance: ' + raceDetails['distance'];
+    raceGenderDisplay.innerText = 'Gender: ' + raceDetails['gender'];
 }
 
 const clearTable = () => {
@@ -55,24 +92,41 @@ const clearTable = () => {
 }
 
 fileInput.addEventListener('change', () => {
+    clearTable()
     const reader = new FileReader();
     reader.readAsText(fileInput.files[0]);
     reader.addEventListener('load', () => {
         const raceResult = JSON.parse((reader.result).toString());
         athletesArray = createAthletesArray(raceResult['results']['athletes']);
+        raceDetails = createRaceDetailsArray(raceResult['results'])
+        displayRaceDetails(raceDetails)
         displayAthletes(athletesArray)
     })
 })
 
 bibSort.addEventListener('click', () => {
     clearTable()
-    athletesArray.sort((a,b) => a.bib.localeCompare(b.bib));
+    if (bibSortCount % 2 === 0) {
+        athletesArray.sort((a,b) => b.bib.localeCompare(a.bib));
+    }
+    else {
+        athletesArray.sort((a,b) => a.bib.localeCompare(b.bib));
+    }
+    bibSortCount ++
+    rankSortCount = 1
     displayAthletes(athletesArray)
 })
 
 rankSort.addEventListener('click', () => {
     clearTable();
-    athletesArray.sort((a,b) => a.rank - b.rank);
+    if (rankSortCount % 2 === 0) {
+        athletesArray.sort((a,b) => b.rank - a.rank);
+    }
+    else {
+        athletesArray.sort((a,b) => a.rank - b.rank);
+    }
+    rankSortCount ++
+    bibSortCount = 1
     displayAthletes(athletesArray)
 })
 
